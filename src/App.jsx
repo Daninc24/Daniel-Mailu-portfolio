b72a113cf1046ebbecd286c229f3a3e6c20283e8
Daninc24 <danmailur8@gmail.com>
2025-04-29 12:29:12 +0300

    full responsiveness commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import HeroMain from './components/heroSection/HeroMain'
-import NavbarMain from './components/navbar/NavbarMain'
-import HeroGradient from './components/heroSection/HeroGradient'
-import SubHeroSection from './components/heroSection/SubHeroSection'
-import AboutMeMain from './components/aboutSection/AboutMeMain'
-import SkillMain from './components/skillsSection/SkillMain' // Added import statement for SkillMain
-import SubSkills from './components/skillsSection/SubSkills'
-import ExperienceMain from './components/experienceSection/ExperienceMain'
-import ProjectMain from './components/projectSection/ProjectMain'
-import ContactMeMain from './components/contactMeSection/ContactMeMain'
-import FooterMain from './components/footer/FooterMain'
+import HeroMain from './components/heroSection/HeroMain';
+import NavbarMain from './components/navbar/NavbarMain';
+import HeroGradient from './components/heroSection/HeroGradient';
+import SubHeroSection from './components/heroSection/SubHeroSection';
+import AboutMeMain from './components/aboutSection/AboutMeMain';
+import SkillMain from './components/skillsSection/SkillMain'; // Added import statement for SkillMain
+import SubSkills from './components/skillsSection/SubSkills';
+import ExperienceMain from './components/experienceSection/ExperienceMain';
+import ProjectMain from './components/projectSection/ProjectMain';
+import ContactMeMain from './components/contactMeSection/ContactMeMain';
+import FooterMain from './components/footer/FooterMain';
 
 function App() {
   return (
-    <main className='font-body'>
+    <main className='font-body text-white relative overflow-hidden'>
       <NavbarMain/>
       <HeroMain/>
       <HeroGradient/>
--- src/components/aboutSection/AboutMeText.jsx
+++ src/components/aboutSection/AboutMeText.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AboutMeText = () => {
   return (
-    <div className='flex flex-col md:items-start sm:items-center md:text-left sm:text-center'>
+    <div className='lex flex-col md:items-start sm:items-center md:text-left sm:text-center'>
       <h1 className='text-6xl text-cyan mb-10'>About Me</h1>
       <p className='text-white'>Hi, I'm Daniel Mailu — a passionate Full Stack Developer with a strong foundation in building scalable, user-centric web applications from the ground up. With experience across both front-end and back-end technologies, I specialize in turning complex problems into clean, efficient solutions.
       Whether it's crafting seamless user interfaces or designing robust APIs, I enjoy working across the entire development stack. I’m driven by curiosity, clean code, and the challenge of building tech that makes a real impact.
